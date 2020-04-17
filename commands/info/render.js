150c7b7d01ee439d1a3f801e6d7ee2a475aafef5
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-17 08:53:33 +0700

    Add files via upload

--- commands/info/render.js
+++ commands/info/render.js
@@ -7,12 +7,12 @@ module.exports = {
     timeout: 4000,
 
     run: async (bot, message, args) =>{
-        let worldname = args.join(" ");
+        let worldname = args.join(" ").toLowerCase();
         let image = `https://s3.amazonaws.com/world.growtopiagame.com/${worldname}.png`;
         if(!worldname)
-        return message.channel.send("what world do ya wanna render?")
-        if(!message.attachments.first() ? !message.attachments.proxyURL : null)
-        return message.channel.send("world render doesn't exist/world owner didn't render the world.");
+        return message.channel.send("what world do ya wanna render?");
+        if(image.size > 0)
+        return message.channel.send("world doesn't exist/world owner didn't render the world.")
 
         let render = new MessageEmbed()
         .setTitle("Growtopia World Render")
