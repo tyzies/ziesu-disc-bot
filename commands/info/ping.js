1006f45684efdb56bd41442b336af1d282a89712
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-17 09:58:34 +0700

    Add files via upload

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -2,18 +2,17 @@ const Discord = require('discord.js')
 module.exports = {
     name: "ping",
     category: 'info',
-    timeout: 5000,
-
     description: "Returns latency and API ping",
-    timeout: 10000,
+    timeout: 3000,
+
     run: async (bot, message, args) => {
-         message.channel.send(`🏓 Pinging....`).then(msg=>{
-        const _ = new Discord.MessageEmbed()
-        .setTitle('🏓 Pong!')
-        .setDescription(`Latency: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nBot Latency: ${Math.round(bot.ws.ping)}ms`)
-        .setColor('RANDOM')
-        msg.edit(_);
-        msg.edit("\u200B")
-    })
+        message.channel.send(`🏓 Pinging....`).then(msg => {
+            const _ = new Discord.MessageEmbed()
+                .setTitle('🏓 Pong!')
+                .setDescription(`Latency: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nBot Latency: ${Math.round(bot.ws.ping)}ms`)
+                .setColor('RANDOM')
+            msg.edit(_);
+            msg.edit("\u200B")
+        })
     }
 }
