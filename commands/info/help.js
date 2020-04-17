e57e4cd0cc0fbe1ee42a12fd8e44d644ebf79b2e
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-17 10:47:21 +0700

    Add files via upload

--- commands/info/help.js
+++ commands/info/help.js
@@ -13,7 +13,7 @@ module.exports = {
     run: async (bot, message, args) =>{
         if(args[0] === 'fun'){
             let fEmbed = new MessageEmbed()
-            .setTitle("<a:PepeDance:657497966214512661> Fun Commands")
+            .setTitle("<a:PepeDance:657497966214512661>   Fun Commands")
             .setDescription("``8ball``, ``meme``, ``say``, ``howgay``, ``penis``, ``hug``")
             .setColor("RANDOM")
             .setFooter('made by tyzies#9020')
@@ -22,7 +22,7 @@ module.exports = {
         }
         if(args[0] === 'moderation'){
             let mEmbed = new MessageEmbed()
-            .setTitle("<a:siren:657601372195389461> Moderation Commands")
+            .setTitle("<a:siren:657601372195389461>   Moderation Commands")
             .setDescription("``ban``, ``kick``, ``role <create, delete>``, ``unban``, ``snipe``, ``addrole``, ``removerole``")
             .setColor("RANDOM")
             .setFooter('made by tyzies#9020')
@@ -31,7 +31,7 @@ module.exports = {
         }
         if(args[0] === 'info'){
             let iEmbed = new MessageEmbed()
-            .setTitle('<a:BlobThink:657601443511140353> Info Commands')
+            .setTitle('<a:BlobThink:657601443511140353>   Info Commands')
             .setDescription("``help``, ``ping``, ``avatar``, ``invite``, ``serverinfo``, ``userinfo``, ``fortnite``, ``uptime``, ``corona``, ``render``")
             .setColor("RANDOM")
             .setFooter('made by tyzies#9020')
@@ -40,7 +40,7 @@ module.exports = {
         } 
         if (args[0] === 'text'){
             let tEmbed = new MessageEmbed()
-            .setTitle('<a:owoO:657498056815673344><a:owoW:657498068882685952><a:owoO:657498056815673344> Text Commands')
+            .setTitle('<a:owoO:657498056815673344><a:owoW:657498068882685952><a:owoO:657498056815673344>   Text Commands')
             .setDescription("``embed``, ``orangetext``, ``yellowtext``, ``greentext``, ``bluetext``")
             .setColor("RANDOM")
             .setFooter('made by tyzies#9020')
@@ -49,7 +49,7 @@ module.exports = {
         }
         if (args[0] === 'misc'){
             let miscEmbed = new MessageEmbed()
-            .setTitle('<a:BlurpKek:657601256000323585> Miscellaneous Commands')
+            .setTitle('<a:BlurpKek:657601256000323585>   Miscellaneous Commands')
             .setDescription("``suggestion``")
             .setColor("RANDOM")
             .setFooter('made by tyzies#9020')
@@ -59,13 +59,13 @@ module.exports = {
         else {
             let hEmbed = new MessageEmbed()
             .setAuthor(bot.user.username, bot.user.displayAvatarURL())
-            .setTitle("<:sanswink:699891393958641724> Commands List")
+            .setTitle("<:sanswink:699891393958641724>   Commands List")
             .setDescription(`Prefix: \`${prefix}\``)
-            .addField(`<a:PepeDance:657497966214512661> Fun`, `\`${prefix}help fun\``)
-            .addField("<a:siren:657601372195389461> Moderation", `\`${prefix}help moderation\``)
-            .addField("<a:BlobThink:657601443511140353> Info", `\`${prefix}help info\``)
-            .addField("<a:owoO:657498056815673344><a:owoW:657498068882685952><a:owoO:657498056815673344> Text", `\`${prefix}help text\``)
-            .addField("<a:BlurpKek:657601256000323585> Miscellaneous", `\`${prefix}help misc\``)
+            .addField(`<a:PepeDance:657497966214512661>   Fun`, `\`${prefix}help fun\``)
+            .addField("<a:siren:657601372195389461>   Moderation", `\`${prefix}help moderation\``)
+            .addField("<a:BlobThink:657601443511140353>   Info", `\`${prefix}help info\``)
+            .addField("<a:owoO:657498056815673344><a:owoW:657498068882685952><a:owoO:657498056815673344>   Text", `\`${prefix}help text\``)
+            .addField("<a:BlurpKek:657601256000323585>   Miscellaneous", `\`${prefix}help misc\``)
             .setFooter("Bugs? Contact tyzies#9020 | Command Display inspired by Dank Memer")
             .setColor("0x00b9fc")
             message.channel.send(hEmbed);
