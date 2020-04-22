9e32b4f9229202d68260e1f9d63ad60209f3748d
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-22 15:43:52 +0700

    Add files via upload

--- commands/info/version.js
+++ commands/info/version.js
@@ -14,7 +14,7 @@ module.exports = {
     run: async (bot, message, args) => {
         let versionEmbed = new MessageEmbed()
             .setTitle(`${bot.user.username} Updates, Change Logs, Version`)
-            .setDescription(`<a:lr:698779764823031819> Version: v${version}\nPrefix: \`${prefix}\`\n\n<a:lr:698779764823031819> **Updates/Change Logs:**\nNone.`)
+            .setDescription(`<a:lr:698779764823031819> Version: v${version}\nPrefix: \`${prefix}\`\n\n<a:lr:698779764823031819> **Updates/Change Logs:**\nCorona Command Updated! \`t.corona <country, all>\``)
             .setFooter(`${bot.user.tag}`, bot.user.displayAvatarURL())
 
         message.channel.send(versionEmbed);
