511e52f0b76f3c60d8c0b6317cfa293ad62d37f4
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-22 16:15:36 +0700

    Add files via upload

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -8,12 +8,11 @@ module.exports = {
 
     run: async (bot, message, args) =>{
         let inviteEmbed = new MessageEmbed()
-        .setAuthor("tyzies.js Invite", bot.user.displayAvatarURL())
+        .setAuthor(`${bot.user.username} Invite`, bot.user.displayAvatarURL())
         .setThumbnail(bot.user.displayAvatarURL())
         .setDescription(`<a:lr:698779764823031819> [Invite Link](https://discordapp.com/oauth2/authorize?client_id=694178512843702362&scope=bot&permissions=2146958847)\nVersion: v1.5\nPrefix: \`t.\`\n\n<a:lr:698779764823031819> **IMPORTANT NOTICES:**\nStill *in beta* so expect bugs.`)
         .setColor(0x03fc17)
         .setTimestamp();
-        message.author.send(inviteEmbed);
-        message.channel.send("Sent to your DMs.");
+        message.channel.send(inviteEmbed);
     }
 }
