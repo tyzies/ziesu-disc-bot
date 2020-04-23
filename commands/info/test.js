cbd8c9c46f00d6f47bbf2bf5e301cab811f99398
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-23 18:20:08 +0700

    Create test.js

---
+++ commands/info/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "test"
+  
+  run: async (bot, message, args){
+     let reason = args.join(" ")
+     
+     if(!reason){
+       reason == "not ok"
+      }
+      
+      message.channel.send(`Reason: ${reason}`);
+  }
+}
