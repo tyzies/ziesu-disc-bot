5a770cbe2fdd31d7d39f1fd5e61720272a4420e6
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-24 10:06:00 +0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sleep: function(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
+}
