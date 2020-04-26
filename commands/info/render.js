dbc63b30eedec7c4768855a9569da64d3692f138
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-26 16:15:59 +0700

    Add files via upload

--- commands/info/render.js
+++ commands/info/render.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch')
+const request = require('request')
 
 module.exports = {
     name: 'render',
@@ -12,10 +12,6 @@ module.exports = {
         let worldname = args.join(" ").toLowerCase();
         let image = `https://s3.amazonaws.com/world.growtopiagame.com/${worldname}.png`;
 
-        let idk = await fetch(image).then(response => response.json());
-
-        if(!idk)
-        return message.channel.send('world dont exist')
 
         if (!worldname)
             return message.channel.send("what world do ya wanna render?");
@@ -25,7 +21,7 @@ module.exports = {
         let render = new MessageEmbed()
             .setTitle("Growtopia World Render")
             .setDescription(`<a:lr:698779764823031819> World Name: ${worldname.toUpperCase()}\n<a:no:699881296515825664> No image = World doesn't exist/World owner didn't render the world yet.`)
-            .setImage(idk)
+            .setImage(image)
             .setColor("RANDOM")
             .setFooter("World Render")
             .setTimestamp()
