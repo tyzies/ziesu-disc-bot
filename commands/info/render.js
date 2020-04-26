54dd0aabf6ec3956ee92ea9358348981cdc2b780
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-26 15:25:35 +0700

    Add files via upload

--- commands/info/render.js
+++ commands/info/render.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-const request = require('request');
 
 module.exports = {
     name: 'render',
@@ -11,14 +10,22 @@ module.exports = {
     run: async (bot, message, args) => {
         let worldname = args.join(" ").toLowerCase();
         let image = `https://s3.amazonaws.com/world.growtopiagame.com/${worldname}.png`;
+        if (!worldname)
+            return message.channel.send("what world do ya wanna render?");
+        if (image.thumbnail = null)
+            return message.channel.send("World doesn't exist/World owner didn't render the world yet.");
+        if (args[1])
+            return message.channel.send("woah slow down. no spaces please.");
 
-        request(image, function (err, response, body) {
-            if (err) console.log(err)
-            body = JSON.parse(body)
-            urlimage = body(image)
-        });
+        let render = new MessageEmbed()
+            .setTitle("Growtopia World Render")
+            .setDescription(`<a:lr:698779764823031819> World Name: ${worldname.toUpperCase()}\n<a:no:699881296515825664> No image = World doesn't exist/World owner didn't render the world yet.`)
+            .setImage(image)
+            .setColor("RANDOM")
+            .setFooter("Growtopia World Render")
+            .setTimestamp()
 
-        console.log(request)
+        message.channel.send(render);
 
     }
 }
