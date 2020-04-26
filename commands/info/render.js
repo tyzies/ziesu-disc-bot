7119fdb1996bcc708df0f1f0b22e7e1e3be06fa0
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-26 15:59:37 +0700

    Add files via upload

--- commands/info/render.js
+++ commands/info/render.js
@@ -13,7 +13,7 @@ module.exports = {
         let image = `https://s3.amazonaws.com/world.growtopiagame.com/${worldname}.png`;
 
         request(image, function(err, response, body){
-            console.log(image.media_type);
+            console.log(image.png);
         })
 
         if (!worldname)
