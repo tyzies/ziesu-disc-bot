dcdd5bc3defc3548c1edabd01ac923f7a0a9faef
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 18:13:26 +0700

    Add files via upload

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -1,4 +1,4 @@
-const { kill } = require('../../assetts')
+const { kill } = require('../../assets/randoms/kill.json')
 
 module.exports = {
 
@@ -14,21 +14,10 @@ module.exports = {
         if (killMember.id === '296627878752419850') return message.channel.send("nope, he's too overpowered af i could get killed instead lmao");
         if (killMember.id === '694178512843702362') return message.channel.send("no thanks.");
 
-        let kill = [
-            `**${killMember.username}** died from falling on a building, so sad.`,
-            `a car passed and accidentally crashed into **${killMember.username}**`,
-            `theres snakes in **${killMember.username}**'s house and one of the snakes bite **${killMember.username}**`,
-            `**${killMember.username}** tried to swim in lava.`,
-            `**${killMember.username}** had a rough life and decided to end it. :(`,
-            `**${killMember.username}** got sliced by Tanjirou's water breathing technique.`,
-            `Zenitsu slept and sliced **${killMember.username}** accidentally. whoopsies.`,
-            `Inosuke's beast breathing is too overpowered and killed **${killMember.username}.**`,
-            `**${killMember.username}** died from falling.`,
-            `**${killMember.username}** is killed by ${message.author}.`,
-            `**${killMember.username}** fell into the void.`
-        ]
         let kills = kill[(Math.random() * kill.length)];
 
         message.channel.send(kills)
+        .replace(/\$mention/g, killMember.username)
+        .replace(/\$author/g, message.author.username)
     }
 }
