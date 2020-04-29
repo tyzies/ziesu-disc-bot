298bd8ef28e10846a3e3a1a7e86ed18de0ac0f05
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 10:07:47 +0700

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js')
 const api = require('imageapi.js');
+const { sleep } = require('../../functions')
 
 module.exports = {
     name: "meme",
@@ -18,16 +19,16 @@ module.exports = {
         let subreddit = subreddits[Math.floor(Math.random() * subreddits.length)]
         let img = await api(subreddit)
 
-        message.channel.send("lol wait ok").then(msg =>{
-        
+        let msg = await message.channel.send("fetching memes..")
+        await sleep(2000)
+
         const memeEmbed = new MessageEmbed()
         .setTitle(`r/${subreddit}`)
         .setURL(`https://reddit.com/r/${subreddit}`)
         .setColor("RANDOM")
         .setImage(img)
 
-        msg.edit(memeEmbed)
-        })
+        msg.edit(memeEmbed);
         
     }
 }
