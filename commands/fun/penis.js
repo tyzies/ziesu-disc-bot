92e527cec4398e62b109d986ab500c9d5f97ec35
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 14:13:06 +0700

    Update penis.js

--- commands/fun/penis.js
+++ commands/fun/penis.js
@@ -16,7 +16,7 @@ module.exports = {
 
 
         let peepeeuser = new MessageEmbed()
-            .setTitle(`**peepees size machine**`)
+            .setTitle(`**peepee size machine**`)
             .setDescription(`${ppUser.username}'s penis\n` + penis[result])
             .setColor(0xff0000)
         message.channel.send(peepeeuser)
