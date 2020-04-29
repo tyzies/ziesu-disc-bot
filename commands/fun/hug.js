553d12e8704c85e783055daaccc887baa46e9a7e
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 19:37:55 +0700

    Add files via upload

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require('discord.js');
+const { hug } = require('../../assets/hugurl.json');
+const { randomInArray } = require('../../functions');
 
 module.exports = {
     name: 'hug',
@@ -7,21 +9,8 @@ module.exports = {
     timeout: 3000,
 
     run: async (bot, message, args) => {
-        let hug = [
-            'https://i.imgur.com/Ltmb8aa.gif',
-            'https://i.imgur.com/c3WzMZu.gif',
-            'https://i.imgur.com/XOYzWBp.gif',
-            'https://i.imgur.com/QDkZ3QK.gif',
-            'https://i.imgur.com/mvDeiFR.gif',
-            'https://i.imgur.com/VvoB5Mv.gif',
-            'https://media.giphy.com/media/llmZp6fCVb4ju/giphy.gif',
-            'https://media.giphy.com/media/EvYHHSntaIl5m/giphy.gif',
-            'https://media.giphy.com/media/16bJmyPvRbCDu/giphy.gif',
-            'https://media.giphy.com/media/QbkL9WuorOlgI/giphy.gif',
-            'https://media.giphy.com/media/6uEE79cXjssla/giphy.gif'
-        ];
 
-        let hugs = hug[Math.floor(Math.random() * hug.length)];
+        let hugs = randomInArray(hug)
 
         let hugPerson = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!hugPerson) return message.channel.send('who do you wanna hug? uwu');
