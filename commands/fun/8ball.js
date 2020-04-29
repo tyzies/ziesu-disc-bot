1d10bb37d1fb77fcedb1efa6ce830b0b090f996a
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 19:29:32 +0700

    Add files via upload

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,25 +1,20 @@
-module.exports={
+const { response } = require('../../assets/arrays/8ball.json')
+const { randomInArray } = require('../../functions')
+
+module.exports = {
     name: "8ball",
     description: "There is a big chance I insult you!",
     category: "fun",
     aliases: ["ask"],
     timeout: 5000,
-    
-    run: async(bot,message,args)=>{
-        let question = message.content.slice(bot.prefix.length+6)
-        if(!question)return message.channel.send(`You did not specify your question!`)
-        else{
-            let responses=[
-                "pfft, try me.",
-                "lmao nope",
-                "ofc yes",
-                "yep",
-                "try asking me again. i'm bot-TIRED. get it?",
-                "nah not in a million years."
-            ]
-            let response = responses[Math.floor(Math.random()*(responses.length)-1)]
-            
-            message.channel.send([response]);
+
+    run: async (bot, message, args) => {
+        let question = message.content.slice(bot.prefix.length + 6)
+        if (!question) return message.channel.send(`You did not specify your question!`)
+        else {
+            let responses = randomInArray(response)
+
+            message.channel.send(responses);
         }
     }
 }
