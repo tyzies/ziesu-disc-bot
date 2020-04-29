c2974fefb16ec84612e55ac76f95af40275c9e79
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-29 14:33:26 +0700

    Add files via upload

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -1,4 +1,7 @@
 const { MessageEmbed } = require('discord.js');
+const pref = require('../../config.json');
+const prefix = pref.prefix;
+const version = "v1.5"
 
 module.exports = {
     name: 'invite',
@@ -10,7 +13,7 @@ module.exports = {
         let inviteEmbed = new MessageEmbed()
         .setAuthor(`${bot.user.username} Invite`, bot.user.displayAvatarURL())
         .setThumbnail(bot.user.displayAvatarURL())
-        .setDescription(`<a:lr:698779764823031819> [Invite Link](https://discordapp.com/oauth2/authorize?client_id=694178512843702362&scope=bot&permissions=2146958847)\nVersion: v1.5\nPrefix: \`t.\`\n\n<a:lr:698779764823031819> **IMPORTANT NOTICES:**\nStill *in beta* so expect bugs.`)
+        .setDescription(`<a:lr:698779764823031819> [Invite Link](https://discordapp.com/oauth2/authorize?client_id=694178512843702362&scope=bot&permissions=2146958847)\nVersion: ${version}\nPrefix: \`${prefix}\`\n\n<a:lr:698779764823031819> **IMPORTANT NOTICES:**\nStill *in beta* so expect bugs.`)
         .setColor(0x03fc17)
         .setTimestamp();
         message.author.send(inviteEmbed);
