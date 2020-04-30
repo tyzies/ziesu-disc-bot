2a0ebabc2ff6fc319bbf3db2c75959b112b51a35
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-30 15:04:38 +0700

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,17 +4,5 @@ const prefix = pref.prefix
 module.exports = bot => {
     console.log("im ready yeeeeet");
 
-    let statuses = [
-        "in beta!",
-        `prefix: ${prefix}`,
-        "now with 24/7 hosting!",
-        `${bot.guilds.cache.size} servers`,
-        `${bot.users.cache.size} members`
-    ]
-
-    setInterval(function () {
-        let status = statuses[Math.floor(Math.random() * statuses.length)]
-        bot.user.setActivity(status, { type: "WATCHING" });
-    }, 5000)
-
+    bot.user.setActivity(`${prefix}help`, { type: "STREAMING", url: 'https://twitch.tv/jemplix' });
 }
