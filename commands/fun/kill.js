c99775e1a04d6f050ca5a0618454489f22d713d4
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-30 12:16:11 +0700

    Add files via upload

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -1,34 +1,23 @@
 const { randomInArray } = require('../../functions')
+const { kill } = require('../../assets/arrays/kill.json')
 
 module.exports = {
 
     name: 'kill',
     description: 'sends kill message to a user',
     usage: 'tkill <user>',
-    aliases: [`bunuh`],
+    aliases: ["bunuh"],
     timeout: 5000,
 
     run: async (bot, message, args) => {
         let killMember = message.mentions.users.first();
-        if (!killMember) return message.reply(`who r u going to kill. tag them or i can't kill them.`);
-        if (killMember.id === '296627878752419850') return message.channel.send(`nope, he's too overpowered af i could get killed instead lmao`);
-        if (killMember.id === '694178512843702362') return message.channel.send(`no thanks.`);
+        if (!killMember) return message.reply("who r u going to kill. tag them or i can't kill them.");
+        if (killMember.id === '296627878752419850') return message.channel.send("nope, he's too overpowered af i could get killed instead lmao");
+        if (killMember.id === '694178512843702362') return message.channel.send("no thanks.");
 
-        let kill = [
-            `**${killMember.username}** died from falling on a building, so sad.`,
-            `a car passed and accidentally crashed into **${killMember.username}**`,
-            `theres snakes in **${killMember.username}**'s house and one of the snakes bite **${killMember.username}**`,
-            `**${killMember.username}** tried to swim in lava.`,
-            `**${killMember.username}** had a rough life and decided to end it. :(`,
-            `**${killMember.username}** got sliced by Tanjirou's water breathing technique.`,
-            `Zenitsu slept and sliced **${killMember.username}** accidentally. whoopsies.`,
-            `Inosuke's beast breathing is too overpowered and killed **${killMember.username}.**`,
-            `**${killMember.username}** died from falling.`,
-            `**${killMember.username}** is killed by ${message.author.username}.`,
-            `**${killMember.username}** fell into the void.`
-        ]
-        let kills = randomInArray(kill)
+        let killz = randomInArray(kill)
+        let killss = killz.replace(/\$mention/g, `${killMember.username}`).replace(/\$author/g, `${message.author.username}`).replace(/"/g, ``);
 
-        message.channel.send(kills);
+        message.channel.send(killss);
     }
 }
