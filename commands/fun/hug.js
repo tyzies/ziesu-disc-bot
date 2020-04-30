8c28184bb7fe66942c4d3116b0240e9be0a7c0d2
tyzies <57895018+tyzies@users.noreply.github.com>
2020-04-30 12:46:54 +0700

    Add files via upload

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -15,6 +15,7 @@ module.exports = {
         let hugPerson = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!hugPerson) return message.channel.send('who do you wanna hug? uwu');
         if (hugPerson.id === message.author.id) return message.channel.send("you can't hug yourself, bud.");
+        if (hugPerson.id === '694178512843702362') return message.channel.send("u-u-uhm.. he-heh.. th-thanks b-but you sh-shou-should h-h-hug somebody else *blush*")
 
         let hugEmbed = new MessageEmbed()
             .setTitle(`${message.author.username} hugged ${hugPerson.user.username}. how cute.`)
