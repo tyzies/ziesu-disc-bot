9bb113ec29ffddca10d99b7980d983c70b9c9555
tyzies <57895018+tyzies@users.noreply.github.com>
2020-05-01 16:37:59 +0700

    Add files via upload

---
+++ commands/fun/slap.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js');
+const { randomInArray } = require('../../functions');
+const { slapURL, slapMessage } = require('../../assets/arrays/slap.json');
+
+module.exports = {
+    name: 'slap',
+    description: 'slap someone!',
+    usage: '<command> @user',
+    timeout: 5000,
+
+    run: async (bot, message, args) =>{
+        let zies = message.channel;
+
+        let user = message.mentions.users.first();
+        if(!user) return zies.send("who do you want to slap? i'll do the job.");
+
+        let slapgif = randomInArray(slapURL)
+        let slapmsg = randomInArray(slapMessage)
+        let slapMsg = slapmsg.replace(/\$mention/g, `${user.username}`)
+        .replace(/\$author/g, `${message.author.username}`)
+        .replace(/\""/g, ``)
+
+        let embed = {
+            color: "RANDOM",
+            title: `${slapMsg}`,
+            author: {
+                name: message.author.username,
+                icon_url: message.author.displayAvatarURL()
+            },
+            image: {
+                url: slapgif
+            }
+        }
+
+        zies.send({ embed: embed });
+    }
+}
