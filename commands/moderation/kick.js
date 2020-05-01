1c3b30ad472f46f25c31f63b3e0757313a00cb6a
tyzies <57895018+tyzies@users.noreply.github.com>
2020-05-01 08:12:34 +0700

    Add files via upload

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -10,13 +10,14 @@ module.exports={
         if(!args[0])return message.channel.send(`who d'ya wanna kick? (provide their ID.)`)
         let User = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
         if(!User)return message.channel.send(`that user is not in the server dum dum`)
-        if(User.permissions.has("ADMINISTRATOR", "BAN_MEMBERS")) return message.channel.send("cant kick this user. they're godly.")
+        if(User.permissions.has("KICK_MEMBERS")) return message.channel.send("cant kick this user. they're godly.")
+        if(!message.guild.me.hasPermission(["KICK_MEMBERS"])) return message.channel.send("i don't have the kick members permission.")
         let Reason = message.content.split(`!kick ${User.id} `)
         if(!args[1])return message.channel.send(`why do you wanna kick this butthead? tell me. (reason required.)`)
         if(!Reason) return message.channel.send(`why do you wanna kick this butthead? tell me. (reason required.)`)
         if(!message.member.permissions.has("KICK_MEMBERS"))return message.channel.send(`u don't have kick perms, kid. get outta here`)
         if(User.user.id === '296627878752419850') return message.channel.send('cannot kick him. hes my owner afterall')
-        User.user.id.kick(Reason)
+        User.kick(Reason)
         
         message.channel.send(`<:BlobSaluteBan:657602844664397844> kicked ${bot.users.cache.get(User.id).username}. adios! <a:PepeHello:657601419016405022>`);
     }
