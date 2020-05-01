f301f8bcf4d441e9828be70b108520f3668d5f6c
tyzies <57895018+tyzies@users.noreply.github.com>
2020-05-01 15:54:36 +0700

    Add files via upload

--- functions.js
+++ functions.js
@@ -12,5 +12,18 @@ module.exports = {
     
         var randomIndex=Math.floor(Math.random()*anArray.length)	
         return anArray[randomIndex];
+    },
+    mentions: function (mention) {
+        // The id is the first and only match found by the RegEx.
+        const matches = mention.match(/^<@!?(\d+)>$/);
+    
+        // If supplied variable was not a mention, matches will be null instead of an array.
+        if (!matches) return;
+    
+        // However the first element in the matches array will be the entire mention, not just the ID,
+        // so use index 1.
+        const id = matches[1];
+    
+        return client.users.cache.get(id);
     }
 }
