38c15447434ad133085a973b41874f4e789869ad
tyzies <57895018+tyzies@users.noreply.github.com>
2020-07-01 09:44:13 +0700

    added license to index

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ *  ziesu Discord Bot, a bot made for fun, for everyone!
+ *  Copyright (C) 2020 ziesu Discord Bot
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const Discord = require("discord.js");
 const fs = require("fs");
 const config = require('./config.json')
